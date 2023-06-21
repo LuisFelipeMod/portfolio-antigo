10eea25b7f1a542c5d0f313f3fcf958f8c3a4350
Luis Modesto <luisfelipetecno@gmail.com>
2023-06-20 23:00:40 -0300

    feat: update

--- .next/static/8FNRXDECZMnAsdcCvTLuD/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-c0773b778055bddb.js","static/chunks/pages/index-8985b42f21d86f32.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
