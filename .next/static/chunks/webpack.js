9ccffa8549d05707d4b5addce6807f0309deb9f7
Luis Modesto <luisgmodesto12@gmail.com>
2023-09-03 22:49:35 -0300

    feat: new changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d446fad523516cd7"; }
+/******/ 		__webpack_require__.h = function() { return "2fd76a735685f1f0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
