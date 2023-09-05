de851f52e84d5cd209b291357c12aad3886f9c1f
Luis Modesto <luisgmodesto12@gmail.com>
2023-09-04 21:38:48 -0300

    feat: phone and email infos

--- components/curriculum.js
+++ components/curriculum.js
@@ -81,6 +81,40 @@ export default function Curriculum({children, home}){
                         Desenvolvedor Front-End</motion.h2>
                     <ScrollBehavior/>
                     <SocialMedias/>
+                    <motion.div
+                        className={styles.contactMe}
+                        initial = {{
+                            transform: "scale3d(1, 0, 1)",
+                            opacity: 0
+                          }}
+                          animate = {{
+                              transform: "scale3d(1, 1, 1)",
+                              opacity: 1
+                          }}
+                          transition ={{
+                            ease: "linear",
+                            delay: 2.1,
+                            duration: 0.2,
+                          }}
+                    >
+                        <a href='mailto:luisgmodesto12@gmail.com'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width={20} height={20} viewBox="0 0 24 24" stroke-width="1.5" stroke="#010626">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z" />
+                            </svg>
+                            <span>
+                                luisgmodesto12@gmail.com
+                            </span>
+                        </a>
+                        <a href='telto:+5514998084367'>
+                            <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 24 24" stroke-width="1.5" stroke="#010626">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                            </svg>
+
+                            <span>
+                                (14) 998084367 
+                            </span>
+                        </a>
+                    </motion.div>
                 </div>
                 <motion.div 
                     className={styles.section}
