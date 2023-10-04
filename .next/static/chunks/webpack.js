36ef3d0922c40aaa775dafb5b8e94a7e42a65f32
LuisFelipeMod <luisfelipetecno@gmail.com>
2023-10-04 18:23:22 -0300

    build: packages changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "885eb5fbc28a39a3"; }
+/******/ 		__webpack_require__.h = function() { return "4a46f4437f26ba0a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
